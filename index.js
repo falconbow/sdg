e61a43d989b2309e7ade64c3b043c4439fdf1142
Nikita Ruseev <ruseev.nikita@gmail.com>
2025-09-03 10:22:44 +0400

    feat: add form validation

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const popupElement = document.querySelector(".popup_wrapper");
 const landingWrapper = document.querySelector(".landing_wrapper");
 const cross = document.querySelector(".cross");
-console.log(popupElement.classList);
+const email = document.querySelector(".form_email");
+const password = document.querySelector(".form_password");
+const emailMessage = document.querySelector(".email_message");
+const passwordMessage = document.querySelector(".password_message");
+const emailErrIcon = document.querySelector(".email_error-icon");
+const passwordErrIcon = document.querySelector(".password_error-icon");
+
+const url = "https://api.dating.com/identity";
 
 const showPopup = () => {
   landingWrapper.classList.add("blurred");
@@ -17,12 +24,56 @@ const isPopupClosed = () => {
   return popupElement.classList.contains("hidden");
 };
 
-document.addEventListener("click", (event) => {
+const setInputError = (field) => {
+  if (field === "email") {
+    email.classList.add("form_email-error");
+    emailErrIcon.classList.remove("hidden");
+    emailMessage.classList.remove("hidden");
+  }
+  if (field === "password") {
+    password.classList.add("form_password-error");
+    passwordErrIcon.classList.remove("hidden");
+    passwordMessage.classList.remove("hidden");
+  }
+};
+
+const areFormsValid = () => {
+  if (email.validity.valid && password.value.length >= 8) {
+    return true;
+  }
+};
+
+document.addEventListener("click", async (event) => {
   if (event.target.classList.contains("signup_button")) {
     showPopup();
   }
   if (event.target.classList.contains("cross")) {
     closePopup();
+  }
+
+  if (event.target.classList.contains("form_submit")) {
+    if (!email.validity.valid) {
+      setInputError("email");
+    }
+    if (password.value.length < 8 && password.value.length !== 0) {
+      setInputError("password");
+    }
+    if (areFormsValid()) {
+      try {
+        const response = await fetch(url, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email.value,
+            password: password.value,
+          }),
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
   }
 });
 
